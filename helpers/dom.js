499d71dfef26225324e527a94cecc19c4c3e344e
sashman <sashman90@gmail.com>
2021-01-16 11:53:10 +0000

    Resetting to use template

--- helpers/dom.js
+++
@@ -1,4 +0,0 @@
-export const getCurretReftHeight = element => {
-  const { current } =  element || {}
-  return  (current ? current.getBoundingClientRect().height : 0)
-}
--- layouts/WithSidebar/WithSidebar.styles.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles'
-
-export default makeStyles((theme) => ({
-  gridItem: {
-    display: 'inlineBlock',
-    border: `solid 1px ${theme.palette.grey}`
-  }}
-))
